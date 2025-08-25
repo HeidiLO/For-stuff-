afbad12cd5c1c0a5a12c7d149df60b4a437079f8
HeidiLO <Osburn@s.dcsdk12.org>
2025-08-25 16:00:01 -0600

    here

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,20 @@
-//@ts-check
-/** @type {HTMLCanvasElement} */
-//@ts-ignore canvas is an HTMLCanvasElement
-const canvas = document.getElementById("game-canvas");
+let single = document.getElementById("1Point");
+let five = document.getElementById("5Point")
+let ten = document.getElementById("10Point")
+let output = document.getElementById("total");
+let total = 0;
 
-// sets the type of the canvas context to 2D
-/** @type {CanvasRenderingContext2D} */
-//@ts-ignore ctx is a CanvasRenderingContext2D
-const ctx = canvas.getContext("2d");
+single.addEventListener("click", function () {
+    let point = total += 1;
+    output.textContent = point
+});
 
-// Set the drawing resolution (not the same as CSS size)
-canvas.width = 800;
-canvas.height = 600;
+five.addEventListener("click", function () {
+    let points = total += 5;
+    output.textContent = points
+});
+
+ten.addEventListener("click", function () {
+    let tens = total += 10;
+    output.textContent = tens
+});
